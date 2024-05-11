f7a036b3c9df1a93c5b4d4b5a7400fb7764911f1
Mario Megias <mariomegias@protonmail.com>
2024-05-11 22:08:25 +0200

    Change signature to achieve a better design

--- software/FUENTES/inc/local_search.hpp
+++ software/FUENTES/inc/local_search.hpp
@@ -3,23 +3,23 @@
 
 #include "metaheuristics.hpp"
 #include "random.hpp"
+#include <cmath>
 
 class Local_search : public Metaheuristics
 {
 private:
     static const double MEAN;
     static const double VARIANCE;
-    static const double MAX_EVALUATIONS;
+    static const unsigned MAX_EVALUATIONS;
 
-    unsigned max_neighbors_to_generate;
     normal_distribution<double> normal;
 
-    void gen_neighbour(const vector<double> & actual_solution, vector<double> & neighbour, unsigned int pos, normal_distribution<double> & normal) const;
+    void gen_neighbour(const vector<double> & actual_solution, vector<double> & neighbor, unsigned pos);
     void compute_weights() override;
 
 public:
     Local_search(const string & name, const Data * training, long seed);
-    void do_local_search(vector<double> & actual_solution, double & fitness_act_sol, unsigned & num_evaluations, unsigned max_neighbors_gen);
+    unsigned do_local_search(vector<double> & actual_solution, double & fitness_act_sol, unsigned max_neighbors_gen, unsigned evaluations_limit);
 };
 
 #endif // ifndef LOCAL_SEARCH_HPP
--- software/FUENTES/src/local_search.cpp
+++ software/FUENTES/src/local_search.cpp
@@ -2,7 +2,7 @@
 
 const double Local_search::MEAN = 0.0;
 const double Local_search::VARIANCE = 0.3;
-const double Local_search::MAX_EVALUATIONS = 15000;
+const unsigned Local_search::MAX_EVALUATIONS = 15000;
 
 using Random = effolkronium::random_static;
 
@@ -10,69 +10,69 @@ Local_search::Local_search(const string & name, const Data * training, long seed
 : Metaheuristics(name, training)
 {
     Random::seed(seed);
-    this->max_neighbors_to_generate = 20 * num_attributes;
     this->normal = normal_distribution<double>(MEAN, sqrt(VARIANCE));
 }
 
-void Local_search::gen_neighbour(const vector<double> & actual_solution, vector<double> & neighbour, unsigned int pos, normal_distribution<double> & normal) const
+void Local_search::gen_neighbour(const vector<double> & actual_solution, vector<double> & neighbor, unsigned pos)
 {
-    neighbour = actual_solution;
-    neighbour[pos] += Random::get(normal);
-    if (neighbour[pos] < 0.0) {
-        neighbour[pos] = 0.0;
-    } else if (neighbour[pos] > 1.0) {
-        neighbour[pos] = 1.0;
+    neighbor = actual_solution;
+    neighbor[pos] += Random::get(normal);
+    if (neighbor[pos] < 0.0) {
+        neighbor[pos] = 0.0;
+    } else if (neighbor[pos] > 1.0) {
+        neighbor[pos] = 1.0;
     }
 }
 
-void Local_search::do_local_search(vector<double> & actual_solution, double & fitness_act_sol, unsigned & num_evaluations, unsigned max_neighbors_gen)
+unsigned Local_search::do_local_search(vector<double> & actual_solution, double & fitness_act_sol, unsigned max_neighbors_gen, unsigned evaluations_limit)
 {
-    if (num_evaluations < MAX_EVALUATIONS)
+    vector<double> neighbor;
+    double fitness_neighbor;
+
+    vector<unsigned> permutation;
+
+    for (int i = 0; i < num_attributes; i++) {
+        permutation.push_back(i);
+    }
+
+    Random::shuffle(permutation);
+
+    unsigned position = 0;
+    unsigned count_evaluations = 0;
+    unsigned num_neighbors_gen = 0;
+
+    while ((num_neighbors_gen < max_neighbors_gen) && (count_evaluations < evaluations_limit))
     {
-        unsigned int num_neighbors_gen = 0;
-
-        vector<double> neighbour;
-        vector<int> permutation;
-
-        for (int i = 0; i < num_attributes; i++) {
-            permutation.push_back(i);
-        }
-
-        unsigned int pos = 0;
-        double fitness_neighbour = -1.0;
-
-        fitness_act_sol = compute_fitness(*training, actual_solution);
-        num_evaluations++;
-
-        Random::shuffle(permutation);
-        while ((num_neighbors_gen < max_neighbors_gen) && (num_evaluations < MAX_EVALUATIONS))
-        {
-            gen_neighbour(actual_solution , neighbour, pos, normal);
-            num_neighbors_gen++;
-            fitness_neighbour = compute_fitness(*training, neighbour);
-            num_evaluations++;
-            if (fitness_neighbour > fitness_act_sol) {
-                actual_solution = neighbour;
-                fitness_act_sol = fitness_neighbour;
-                num_neighbors_gen = 0;
-                Random::shuffle(permutation);
-                pos = 0;
-            } else {
-                pos++;
-                if (pos == num_attributes) {
-                    pos = 0;
-                }
+        gen_neighbour(actual_solution , neighbor, position);
+        num_neighbors_gen++;
+        fitness_neighbor = compute_fitness(*training, neighbor);
+        count_evaluations++;
+        if (fitness_neighbor > fitness_act_sol) {
+            actual_solution = neighbor;
+            fitness_act_sol = fitness_neighbor;
+            num_neighbors_gen = 0;
+            Random::shuffle(permutation);
+            position = 0;
+        } else {
+            position++;
+            if (position == num_attributes) {
+                position = 0;
             }
         }
     }
+
+    return count_evaluations;
 }
 
 void Local_search::compute_weights()
 {
     vector<double> actual_solution = Random::get<vector>(0.0, 1.0, num_attributes);
-    double fitness_act_sol;
-    unsigned num_evaluations = 0;
-    do_local_search(actual_solution, fitness_act_sol, num_evaluations, max_neighbors_to_generate);
+    double fitness_act_sol = compute_fitness(*training, actual_solution);
+    unsigned max_neighbours_gen = 20 * num_attributes;
+    unsigned evaluations_limit = MAX_EVALUATIONS - 1;
+
+    do_local_search(actual_solution, fitness_act_sol, max_neighbours_gen, evaluations_limit);
+
     weights = actual_solution;
 }
 
